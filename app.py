fcd1c71e9fe4864d6334306efef39d7e9d66fcfb
cayu007 <sergiocayuelabravo@gmail.com>
2023-12-11 12:22:08 +0100

    Actualización de app.py y requirements.txt

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask, request, jsonify
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import json
-import os
 
 app = Flask(__name__)
-datos_path = 'datos/nombres.json'
+
+# Cadena de conexión de Azure Blob Storage
+connect_str = "DefaultEndpointsProtocol=https;AccountName=apigptokok;AccountKey=MKh1TJKpb2oJfP+rsgaHwrqqk9dA75xP1H7yktaGuBd528K/HAXLN1brq9h0ZjsmjmJfdMTZanS7+AStmyuASg==;EndpointSuffix=core.windows.net"
+container_name = "datos"
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+container_client = blob_service_client.get_container_client(container_name)
 
 def leer_datos():
-    if not os.path.exists(datos_path):
+    try:
+        blob_client = container_client.get_blob_client(blob="nombres.json")
+        blob_data = blob_client.download_blob().readall()
+        return json.loads(blob_data)
+    except Exception as e:
+        print(e)
         return {"nombres": []}
-    with open(datos_path, 'r') as file:
-        return json.load(file)
 
 def guardar_datos(datos):
-    with open(datos_path, 'w') as file:
-        json.dump(datos, file, indent=4)
+    try:
+        blob_client = container_client.get_blob_client(blob="nombres.json")
+        blob_client.upload_blob(json.dumps(datos), overwrite=True)
+    except Exception as e:
+        print(e)
 
 @app.route('/crearNombre', methods=['POST'])
 def crear_nombre():
@@ -23,7 +34,7 @@ def crear_nombre():
         return jsonify({"error": "Nombre es requerido"}), 400
     
     datos = leer_datos()
-    datos['nombres'].append(nombre)
+    datos.setdefault('nombres', []).append(nombre)
     guardar_datos(datos)
     
     return jsonify({"mensaje": "Nombre almacenado con éxito"})
